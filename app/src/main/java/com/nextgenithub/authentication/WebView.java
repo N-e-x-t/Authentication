003b197ab6604e74ae9f525330b5f6584bf2f787
Jigar Parekh <jigarparekh002@gmail.com>
2017-03-24 00:38:19 +0530

    Changes in webview

--- app/src/main/java/com/nextgenithub/authentication/WebView.java
+++ app/src/main/java/com/nextgenithub/authentication/WebView.java
@@ -1,51 +1,49 @@
 package com.nextgenithub.authentication;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.app.Activity;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.webkit.WebSettings;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.View;
 
-import android.webkit.WebViewClient;
-import android.widget.ProgressBar;
-
-public class WebView extends AppCompatActivity {
+        import android.webkit.WebViewClient;
+        import android.widget.Button;
+        import android.widget.EditText;
 
 
-    private WebView mWebView;
-    ProgressBar progressBar;
+public class WebView extends Activity  {
+    Button b1;
+    EditText ed1;
 
-
+    private WebView wv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
-        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        b1=(Button)findViewById(R.id.button);
+        ed1=(EditText)findViewById(R.id.editText);
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
+        wv1=(WebView)findViewById(R.id.webView);
+        wv1.setWebViewClient(new MyBrowser());
 
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("http://www.google.com");
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = ed1.getText().toString();
 
+                wv1.getSettings().setLoadsImagesAutomatically(true);
+                wv1.getSettings().setJavaScriptEnabled(true);
+                wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                wv1.loadUrl(url);
+            }
+        });
     }
 
-    private void loadUrl(String s) {
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
     }
-
-    private WebSettings getSettings() {
-        return null;
-    }
-
-
 }
-
