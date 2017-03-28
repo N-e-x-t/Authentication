525c14cbd1dd90c7c95eee869bedfcd86cdc71b4
Jigar Parekh <jigarparekh002@gmail.com>
2017-03-28 16:24:50 +0530

    Updated webview

--- app/src/main/java/com/nextgenithub/authentication/WebView.java
+++ app/src/main/java/com/nextgenithub/authentication/WebView.java
@@ -1,49 +1,22 @@
 package com.nextgenithub.authentication;
 
 
-        import android.app.Activity;
-        import android.os.Bundle;
-        import android.view.View;
-
-        import android.webkit.WebViewClient;
-        import android.widget.Button;
-        import android.widget.EditText;
 
 
-public class WebView extends Activity  {
-    Button b1;
-    EditText ed1;
 
-    private WebView wv1;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.webkit.WebView;
+
+public class WebView extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-
-        b1=(Button)findViewById(R.id.button);
-        ed1=(EditText)findViewById(R.id.editText);
-
-        wv1=(WebView)findViewById(R.id.webView);
-        wv1.setWebViewClient(new MyBrowser());
-
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String url = ed1.getText().toString();
-
-                wv1.getSettings().setLoadsImagesAutomatically(true);
-                wv1.getSettings().setJavaScriptEnabled(true);
-                wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-                wv1.loadUrl(url);
-            }
-        });
-    }
-
-    private class MyBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("http://www.google.com");
     }
 }
+
+
